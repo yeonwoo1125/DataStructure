ab8c88576ac6f00bd2adec56c9e21bac332e9694
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-05-14 16:57:06 +0900

    LinkedList 메서드 실습

--- src/list/linkedList/LinkedListMethod.java
+++ src/list/linkedList/LinkedListMethod.java
@@ -1,4 +1,36 @@
 package list.linkedList;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class LinkedListMethod {
+    public static void main(String[] args){
+        List<Integer> linkedList = new LinkedList<>();
+
+        //삽입
+        linkedList.add(1);
+        linkedList.add(1,2);
+
+        //반환
+        System.out.println(linkedList.get(1));
+
+        //초기화
+        linkedList.clear();
+
+        for(int i=1; i<=10; i++)
+            linkedList.add(i);
+
+        //삭제
+        linkedList.remove(4);
+        linkedList.add(3,66);
+
+        //변경
+        linkedList.set(3,10000);
+        System.out.println(linkedList.get(3));
+
+        //출력
+        for(int a : linkedList)
+            System.out.print(a+" ");
+
+    }
 }
