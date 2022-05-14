cecde7469fedf8670a9452f40bfbfb582f02a660
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-05-14 21:14:27 +0900

    Queue 메서드 실습

--- src/queue/QueueMethod.java
+++ src/queue/QueueMethod.java
@@ -1,4 +1,33 @@
 package queue;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QueueMethod {
+    public static void main(String[] args){
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(1);
+        queue.add(2);
+        
+        //peek은 가져만 옴
+        System.out.println(queue.peek());
+        
+        //가져온 후 삭제
+        System.out.println(queue.poll());
+
+        for(int i=1; i<=10; i++)
+            queue.add(i);
+
+        Iterator<Integer> iterator = queue.iterator();
+        while(iterator.hasNext()){
+            System.out.print(iterator.next()+" ");
+        }
+
+        queue.remove();
+        System.out.println();
+        for(int a : queue)
+            System.out.print(a+" ");
+    }
 }
