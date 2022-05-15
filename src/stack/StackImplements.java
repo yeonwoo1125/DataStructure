32403d7c8d22dbfe0b93852938968b00bfbc0552
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-05-15 13:30:33 +0900

    Revert "Stack 메서드 실습"
    This reverts commit a1770cc63c33d678c3f7c8bb5d6a29d37b300fee.

--- src/stack/StackImplements.java
+++ src/stack/StackImplements.java
@@ -1,28 +1,4 @@
 package stack;
 
-import java.util.Stack;
-
 public class StackImplements {
-    public static void main(String[] args){
-        Stack<Integer> stack = new Stack<>();
-
-        for(int i=1; i<=10; i++)
-            stack.push(i);
-
-        System.out.println(stack);
-
-        System.out.println(stack.peek());
-        stack.pop();
-        System.out.println(stack);
-        System.out.println(stack.size());
-
-        stack.clear();
-        System.out.println(stack.isEmpty());
-
-        stack.push(2);
-        stack.push(3);
-        System.out.println(stack.indexOf(3));
-        System.out.println(stack.contains(2));
-        System.out.println(stack.capacity());
-    }
 }
