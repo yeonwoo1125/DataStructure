4ae03659a2a8d3a755401b260884d7eaf54e4808
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-05-15 13:34:39 +0900

    Stack 메서드 실습

--- src/stack/StackMethod.java
+++ src/stack/StackMethod.java
@@ -1,4 +1,27 @@
 package stack;
 
+import java.util.Stack;
+
 public class StackMethod {
+    public static void main(String[] args){
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=1; i<=10; i++){
+            stack.push(i);
+        }
+        System.out.println(stack.size());
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack);
+
+        System.out.println(stack.isEmpty());
+        stack.clear();
+        System.out.println(stack.isEmpty());
+
+        stack.push(1);
+        stack.push(10);
+        System.out.println(stack.contains(1));
+        System.out.println(stack.indexOf(10));
+
+    }
 }
